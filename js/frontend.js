db2133a8bb52bc4b0312e273aef2161321d9a45f
nikulasoskarsson <nikulasoskarsson@gmail.com>
2019-04-02 16:33:40 +0000

    adding comments to frontend.js

--- js/frontend.js
+++ js/frontend.js
@@ -1,103 +1,94 @@
-const imageContainer = document.getElementById("image-container")
-const sortHighest = document.getElementById("highest")
-const sortLowest = document.getElementById("lowest")
-const search = document.getElementById("search")
+const imageContainer = document.getElementById("image-container");
+const sortHighest = document.getElementById("highest");
+const sortLowest = document.getElementById("lowest");
+const search = document.getElementById("search");
 let newCardImages;
-let cardImages;
+let cardImages; // card images global þannig hægt er að vinna með það hvar sem er.
 
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyByIb4TWrs3WcDKgr_aMgyCFVVaPq26iRc",
-    authDomain: "dropar-e12fb.firebaseapp.com",
-    databaseURL: "https://dropar-e12fb.firebaseio.com",
-    projectId: "dropar-e12fb",
-    storageBucket: "dropar-e12fb.appspot.com",
-    messagingSenderId: "983460445073"
+// Firebase starting template sem tengjir firestore bakendann við franmendann hjá okkur
+var config = {
+  apiKey: "AIzaSyByIb4TWrs3WcDKgr_aMgyCFVVaPq26iRc",
+  authDomain: "dropar-e12fb.firebaseapp.com",
+  databaseURL: "https://dropar-e12fb.firebaseio.com",
+  projectId: "dropar-e12fb",
+  storageBucket: "dropar-e12fb.appspot.com",
+  messagingSenderId: "983460445073"
 };
 firebase.initializeApp(config);
-const db = firebase.firestore();
+const db = firebase.firestore(); // Reference við firebase.firestore() þannig við þurfum ekki alltaf að segja firebase.firestore().eitthvadmethod heldur getum sagt db.eitthvadmethod
 
-db.collection("images").get().then(function (querySnapshot) {
-    cardImages = getJson(querySnapshot)
-    displayImages(cardImages)
-});
-getJson = (array) => {
-    let returnJson = [];
-    array.forEach(function(doc){
-        const data = doc.data();
-        returnJson.push(data);
-    })
-    return returnJson;
-}
+db.collection("images") // Nafnið á collection í okkar gagnagrunni
+  .get()
+  .then(function(querySnapshot) {
+    cardImages = getJson(querySnapshot); // kallar í getJson function sem við bjuggum til sem loopar í gegnum querySnapshot objectið og returnar array með þeim upplýsingum.
+    displayImages(cardImages); // Notum nýja cardImage json objectið og setjum inní displayImages til þess að loopa í gegnum það og birta á framendanum
+  });
 
-displayImages = (array)=>{
-    imageContainer.innerHTML = ""
-    array.forEach(image=>{
-        imageContainer.innerHTML += buildImage(image)
+// Function sem tekur inn gögn úr gagnagrunni og setur i í json format (array af objectum) svo hægt sé að vinna með þau og nota high order array functions (sort,filter,map og etc)
+getJson = array => {
+  let returnJson = [];
+  array.forEach(function(doc) {
+    const data = doc.data();
+    returnJson.push(data); // Pushar inn data sem er object inní array-ið
+  });
+  return returnJson; // Functionið skilar núna gögnonum á json formati
+};
 
-    })
-}
-buildImage = (image)=>{
-    return`
+displayImages = array => {
+  imageContainer.innerHTML = ""; // innerhtml-ið hreinsað í byrjun
+  array.forEach(image => {
+    // fyrir hverja einustu mynd
+    imageContainer.innerHTML += buildImage(image); // Kallað í buildImage function með núverandi image sem parameter sem að returnar div með myndinni og það sett í innerhtml á iamgecontater sem birtir allar myndir
+  });
+};
+
+//Build imge tekur inn eitt object í einu og returnar card-i með upplýsingum úr þeim object
+buildImage = image => {
+  return `
     <div>
         <h3>${image.title}</h3>
         <p>${image.description}</p>
     </div>
-    `
-}
+    `;
+};
 
 // Sorting
-sortHighest.onclick = ()=>{
-    console.log(newCardImages)
-    if(typeof newCardImages!== undefined){
-        newCardImages.sort((a,b)=>{
-            return b.rating-a.rating
-            
-        })
-        displayImages(newCardImages)
-    }
-    else{
-        cardImages.sort((a,b)=>{
-            return b.rating-a.rating
-            
-        })
-        displayImages(cardImages)
-    }
-    
-    // console.log(cardImages)
-    // displayImages(cardImages)
-}
-sortLowest.onclick = ()=>{
-    console.log(newCardImages)
-    if(typeof newCardImages!== undefined){
-        newCardImages.sort((a,b)=>{
-            return a.rating-b.rating
-            
-        })
-        displayImages(newCardImages)
-    }
-    else{
-        cardImages.sort((a,b)=>{
-            return a.rating-b.rating
-            
-        })
-        displayImages(cardImages)
-    }
-}
+
+// sortering functions sem að byrja fyrst á að athuga hvort að newCardImages sé undefined sem það er á línu 5 þar sem það er búið til og er þannig þangað til að það er búið að skrifa inn eh í input textboxið.Þá verður newCardImages filteruð gerð af cardImages og því ekki undefined.
+sortHighest.onclick = () => {
+  if (typeof newCardImages !== undefined) {
+    // Ef newCardImages er í notkun viljum við nota það frekar þannig við séum ekki að nota cardImages sem hefur allar myndirnar heldur newCardImages sem hefur bara description sem matchar það sem notandinn er búinn að skrifa
+    newCardImages.sort((a, b) => {
+      return b.rating - a.rating;
+    });
+    displayImages(newCardImages);
+  } else {
+    cardImages.sort((a, b) => {
+      return b.rating - a.rating;
+    });
+    displayImages(cardImages);
+  }
+};
+sortLowest.onclick = () => {
+  if (typeof newCardImages !== undefined) {
+    newCardImages.sort((a, b) => {
+      return a.rating - b.rating;
+    });
+    displayImages(newCardImages);
+  } else {
+    cardImages.sort((a, b) => {
+      return a.rating - b.rating;
+    });
+    displayImages(cardImages);
+  }
+};
 // Search
-search.onkeyup = () =>{
-    let string = search.value.toString()
-    newCardImages = cardImages.filter((currentImage)=>{
-        console.log(currentImage.description)
-        return currentImage.description.includes(string)
-    })
-    displayImages(newCardImages)
-    console.log(string)
-}
 
-
-
-
-
-
+//function sem keryrir í hver einasta skipti sem ýtt er á takka á lylklaborðinu inní search textbox inputinum.
+search.onkeyup = () => {
+  let string = search.value.toString(); // Strengur sem hefur alltaf sama gildi og það sem er verið að skrifa.
+  newCardImages = cardImages.filter(currentImage => {
+    return currentImage.description.includes(string); // Returna bara images þar sem að image.description matchar strengum sem er verið að skrifa
+  });
+  displayImages(newCardImages); // Kallað í displayImages með nýja arrayinu. Það mun síðan hreinsa innerhtml-ið á iamgecontainer og loopa í gegnum og birta þetta í staðinn.
+};
